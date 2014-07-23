202fbb4a1ec8599361d47c195353e8f52fc00586
Shemulinux <shemulworld@gmail.com>
2014-07-24 03:29:57 +0600

    hmm..

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>uniBuddy !</title>
+		<title>RIP uniBuddy !</title>
 		<meta charset="UTF-8">
 		<meta name=description content="">
 		<meta name=viewport content="width=device-width, initial-scale=1">
